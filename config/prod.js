225b3a8c8da36ab6fe08ce06de26dee7acdc20da
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2018-03-25 19:50:10 -0400

    test of prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,14 @@
 // prod.js - production keys here
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
+  googleClientID: '953481300821-3ltfj4k8c7p1rcmj01e02aaamdphf4hh.apps.googleusercontent.com',
+  googleClientSecret: 'RLYi0sMCWVv38Wa5c6GBUEI7',
+  mongoURI: 'mongodb://chao0716:Monkland4410@ds247178.mlab.com:47178/emaily_udemy',
   cookieKey: process.env.COOKIE_KEY
 };
+
+// module.exports = {
+//   googleClientID: process.env.GOOGLE_CLIENT_ID,
+//   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   mongoURI: process.env.MONGO_URI,
+//   cookieKey: process.env.COOKIE_KEY
+// };
