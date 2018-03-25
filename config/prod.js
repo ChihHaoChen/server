fdfc7084b95bda89105315207daf9573b684cf57
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2018-03-25 19:35:04 -0400

    Removal of Facebook Auth

--- config/prod.js
+++ config/prod.js
@@ -3,9 +3,5 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  facebookClientID: process.env.facebookClientID,
-  facebookClientSecret: process.env.facebookClientSecret,
-  facebookProfileURI: process.env.facebookProfileURI,
-  facebookProfileFields: ['id', 'email', 'name']
+  cookieKey: process.env.COOKIE_KEY
 };
