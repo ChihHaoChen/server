f2d6b10135ba7f1838280b386d952e85c19ac3f5
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2018-03-25 20:16:36 -0400

    Update of prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 // prod.js - production keys here
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: 'RLYi0sMCWVv38Wa5c6GBUEI7',
-  mongoURI: 'mongodb://chao0716:Monkland4410@ds247178.mlab.com:47178/emaily_udemy',
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
 
