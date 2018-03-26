83feb46929eb88f15b2ab1f208e994e4c89a7ef5
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2018-03-25 20:13:03 -0400

    3rd trial of prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 // prod.js - production keys here
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleClientSecret: 'RLYi0sMCWVv38Wa5c6GBUEI7',
   mongoURI: 'mongodb://chao0716:Monkland4410@ds247178.mlab.com:47178/emaily_udemy',
   cookieKey: process.env.COOKIE_KEY
 };
