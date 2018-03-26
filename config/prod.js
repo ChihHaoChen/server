3522d4ba442a7457f207644319222609f25a3b13
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2018-03-25 20:11:11 -0400

    2nd trial of prod.js

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: 'mongodb://chihhaochen:password@ds111771.mlab.com:11771/emaily_udemy_prod',
+  mongoURI: 'mongodb://chao0716:Monkland4410@ds247178.mlab.com:47178/emaily_udemy',
   cookieKey: process.env.COOKIE_KEY
 };
 
